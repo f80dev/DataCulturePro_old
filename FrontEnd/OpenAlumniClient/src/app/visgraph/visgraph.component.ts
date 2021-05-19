d0ca3d43c036725fff8f5c02dcf4ebeeceddedfb
f80dev <herve.hoareau@f80.fr>
2021-05-19 18:01:41 +0200

    Correctifs divers notamment pour la vidéo

--- FrontEnd/OpenAlumniClient/src/app/visgraph/visgraph.component.ts
+++ FrontEnd/OpenAlumniClient/src/app/visgraph/visgraph.component.ts
@@ -13,6 +13,7 @@ export class VisgraphComponent implements OnInit {
   private margin = 50;
   private width = screen.availWidth - (this.margin * 2);
   private height = screen.availHeight - (this.margin * 2);
+  name:string="";
 
   constructor(
     public api:ApiService
@@ -30,37 +31,59 @@ export class VisgraphComponent implements OnInit {
 
   draw(data:any,svg:any) {
 
-      var link = svg
-        .selectAll("line")
-        .data(data.edges)
-        .enter()
-        .append("line")
-        .style("stroke", "#aaa")
+    //voir https://github.com/d3/d3-selection#modifying-elements
 
-      // Initialize the nodes
-      var node = svg
-        .selectAll("g.nodes")
-        .data(data.nodes, (d)=>{ return d.id; });
+    var link = svg
+      .selectAll("line")
+      .data(data.edges)
+      .enter()
+      .append("line")
+      .style("stroke", "#aaa")
 
+    // Initialize the nodes
+    var nodeEnter = svg
+      .selectAll("circle")
+      .data(data.nodes)
+      .enter()
+      .append("svg:g")
+      .attr("class", "node")
+      .property("name",function(d) { return d.name;})
+      .on("mouseover", this.mouseover)
 
-      var nodeEnter=node.enter().append("svg:g").attr("class","node");
+    var node = nodeEnter.append("svg:image")
+        .attr("xlink:href",  function(d) { return d.photo;})
+        .attr("x", function(d) { return -25;})
+        .attr("y", function(d) { return -25;})
+        .attr("height", 50)
+        .attr("width", 50)
 
-      nodeEnter
-        .append("svg:circle")
-        .attr("r", 10)
-        .style("fill", "#d01111");
+    nodeEnter.append("svg:text")
+      .text(function(d) { return d.name;})
 
+    var simulation = d3.forceSimulation(data.nodes)
+      .force("link", d3.forceLink().id(function(d:any) { return d.id; }).links(data.edges))
+      .force("charge", d3.forceManyBody().strength(-80))
+      .force("center", d3.forceCenter(this.width / 2, this.height / 2))
+      .on("end",()=>{
+        link
+          .attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
 
-      nodeEnter.append("text")
-      .attr("class", "nodetext")
-      .attr("x", 20)
-      .attr("y", 20 +15)
-      .attr("fill", "#130C0E")
-      .text((d)=> { return d.name; });
-
-
+        node
+          .attr("x", function (d) { return d.x+6; })
+          .attr("y", function(d) { return d.y-6; });
+      });
 
   }
+
+
+
+  mouseover(d){
+    this.name=d.target.name;
+  }
+
 
 
 
